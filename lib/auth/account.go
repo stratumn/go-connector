046cc9e2ece787f3483210c2bef19b99949733dd
Simon Vadée <simon@stratumn.com>
2019-04-23 12:18:15 +0200

    Authorize list of account IDs

--- lib/auth/account.go
+++ lib/auth/account.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/url"
@@ -11,6 +12,8 @@ import (
 // Those are the errors returned by the middleware.
 var (
 	ErrMissingToken = errors.New("an authorization token must be provided")
+
+	ErrUnauthorizedAccount = errors.New("user is not part of the authorized entities")
 )
 
 // Middleware is the interface exposing a middleware function providing authentication.
@@ -21,15 +24,22 @@ type Middleware interface {
 // StratumnAccountMiddleware implements the Middleware interface.
 // It provides authentication using Stratumn Account API.
 type StratumnAccountMiddleware struct {
-	AccountURL string
+	AccountURL         string
+	AuthorizedAccounts []string
+}
+
+// AccountInfo contains the data returned by StratumnAccount `GET /info` call.
+type AccountInfo struct {
+	AccountID       string   `json:"accountId"`
+	OtherAccountIDs []string `json:"otherAccountIds"`
 }
 
 // NewStratumnAccountMiddleware returns a new instance of StratumnAccountMiddleware.
-func NewStratumnAccountMiddleware(accountURL string) (Middleware, error) {
+func NewStratumnAccountMiddleware(accountURL string, authorizedAccounts []string) (Middleware, error) {
 	if _, err := url.ParseRequestURI(accountURL); err != nil {
 		return nil, errors.Wrap(err, "could not instantiate Stratumn Account Auth Middleware")
 	}
-	return &StratumnAccountMiddleware{accountURL}, nil
+	return &StratumnAccountMiddleware{accountURL, authorizedAccounts}, nil
 }
 
 // WithAuth is a middleware function.
@@ -63,8 +73,40 @@ func (s *StratumnAccountMiddleware) WithAuth(next http.HandlerFunc) http.Handler
 			return
 		}
 
+		info := AccountInfo{}
+		err = json.NewDecoder(infoResp.Body).Decode(&info)
+		if err != nil {
+			writeResponse(w, http.StatusUnauthorized, []byte(err.Error()))
+			return
+		}
+
+		err = s.checkAuth(&info)
+		if err != nil {
+			writeResponse(w, http.StatusUnauthorized, []byte(err.Error()))
+			return
+		}
+
 		next.ServeHTTP(w, r)
 	}
+}
+
+func (s *StratumnAccountMiddleware) checkAuth(info *AccountInfo) error {
+	if len(s.AuthorizedAccounts) == 0 {
+		return nil
+	}
+
+	for _, authorized := range s.AuthorizedAccounts {
+		if info.AccountID == authorized {
+			return nil
+		}
+		for _, entityID := range info.OtherAccountIDs {
+			if entityID == authorized {
+				return nil
+			}
+		}
+	}
+
+	return ErrUnauthorizedAccount
 }
 
 func writeResponse(w http.ResponseWriter, statusCode int, response []byte) {
